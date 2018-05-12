1253a6d4b8634af4f6568f7b0097885630f91761
tmosley03 <tlm3dev@gmail.com>
2018-05-12 09:42:58 -0500

    final

---
+++ db/seeds.sql
@@ -0,0 +1,15 @@
+INSERT INTO `
+burgers_db`.`burgers
+`
+(`id`, `burger_name`, `devoured`) VALUES
+('1', 'Cheese Burger', '0');
+INSERT INTO `
+burgers_db`.`burgers
+`
+(`id`, `burger_name`, `devoured`) VALUES
+('2', 'Bacon Burger', '0');
+INSERT INTO `
+burgers_db`.`burgers
+`
+(`id`, `burger_name`, `devoured`) VALUES
+('3', 'Turkey Burger', '0');
